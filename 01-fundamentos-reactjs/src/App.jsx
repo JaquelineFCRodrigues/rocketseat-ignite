c30fd96fc33de32aee4151dff25dfbc57963ccd2
JaquelineFCRodrigues <j.g.rodrigues@accenture.com>
2023-10-11 13:43:57 -0300

    Estrutura da aplicação - CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import './styles.css';
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post 
         author= "Jaqueline Rodrigues" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis error culpa in. Facilis autem quia veritatis necessitatibus commodi optio cum iste, iusto, dicta tenetur nobis voluptas repudiandae velit reiciendis?"
