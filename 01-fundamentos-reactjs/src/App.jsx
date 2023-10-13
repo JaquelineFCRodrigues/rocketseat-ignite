05d40cf9b99276c267609db66248065a92c60e76
JaquelineFCRodrigues <j.g.rodrigues@accenture.com>
2023-10-13 13:04:54 -0300

    Estrutura da aplicação - Componente: Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,28 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        author= "Jaqueline Rodrigues" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis error culpa in. Facilis autem quia veritatis necessitatibus commodi optio cum iste, iusto, dicta tenetur nobis voluptas repudiandae velit reiciendis?"
-      />
-      <Post 
-        author= "Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis error culpa in. Facilis autem quia veritatis necessitatibus commodi optio cum iste, iusto, dicta tenetur nobis voluptas repudiandae velit reiciendis?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author= "Jaqueline Rodrigues" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis error culpa in. Facilis autem quia veritatis necessitatibus commodi optio cum iste, iusto, dicta tenetur nobis voluptas repudiandae velit reiciendis?"
+          />
+          <Post 
+            author= "Diego Fernandes" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facilis error culpa in. Facilis autem quia veritatis necessitatibus commodi optio cum iste, iusto, dicta tenetur nobis voluptas repudiandae velit reiciendis?"
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
